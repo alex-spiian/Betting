714047211fdec4ce63af39e4812ed2fc7656c8b7
Aleksandr Spiian <aleksandr.spiian@icloud.com>
2024-12-05 11:57:19 +0700

    Added CameraAdjuster to control rendering view

---
+++ Assets/Scripts/Camera/CameraAdjuster.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class CameraAdjuster : MonoBehaviour
+{
+    [SerializeField]
+    private BoxCollider2D _level;
+    
+    private Camera _mainCamera;
+
+    private void Start()
+    {
+        _mainCamera = Camera.main;
+        AdjustCameraSizeToFitLevel();
+    }
+
+    private void AdjustCameraSizeToFitLevel()
+    {
+        var levelBounds = _level.bounds;
+
+        var levelHeight = levelBounds.size.y;
+        var levelWidth = levelBounds.size.x;
+
+        var screenAspect = (float)Screen.width / (float)Screen.height;
+        var levelAspect = levelWidth / levelHeight;
+
+        if (levelAspect > screenAspect)
+        {
+            _mainCamera.orthographicSize = levelWidth / screenAspect / 2f;
+        }
+        else
+        {
+            _mainCamera.orthographicSize = levelHeight / 2f;
+        }
+
+        var levelCenter = levelBounds.center;
+        _mainCamera.transform.position = new Vector3(levelCenter.x, levelCenter.y, _mainCamera.transform.position.z);
+    }
+}
